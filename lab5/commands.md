46b145e157c4465ff5f30aab223ad62373bad99c
Shabarshov <108910629+shabarshov@users.noreply.github.com>
2023-12-12 20:25:44 +0000

    lab5

---
+++ lab5/commands.md
@@ -0,0 +1,58 @@
+1.
+gcc -o without_mutex mutex.c -lpthread
+
+// раскомментировал строчки, где используется mutex
+
+gcc -o with_mutex mutex.c -lpthread
+
+Без использования мьютекса:
+Без использования мьютекса, несколько потоков могут одновременно читать и изменять значение переменной common. Это создает гонку данных, что может привести к непредсказуемым результатам. В результате, значение common в конце программы может быть неверным, так как несколько потоков могут перезаписывать его одновременно.
+
+С использованием мьютекса:
+С правильным использованием мьютекса, гарантируется, что только один поток имеет доступ к общей переменной в конкретный момент времени. Это предотвращает гонку данных, обеспечивая корректное и последовательное выполнение кода в критической секции. В результате, значение common в конце программы будет точным, и программа будет вести себя предсказуемо.
+
+2.
+gcc -o factorial factorial.c -lpthread
+
+./factorial -k 10 --pnum=4 --mod=10
+Thread 2 calculated partial result: 12
+Thread 3 calculated partial result: 210
+Thread 4 calculated partial result: 720
+Thread 1 calculated partial result: 2
+10! mod 10 = 0
+
+./factorial -k 10 --pnum=4 --mod=11
+Thread 1 calculated partial result: 2
+Thread 2 calculated partial result: 12
+Thread 3 calculated partial result: 210
+Thread 4 calculated partial result: 720
+10! mod 11 = 10
+
+./factorial -k 10 --pnum=4 --mod=17
+Thread 1 calculated partial result: 2
+Thread 2 calculated partial result: 12
+Thread 4 calculated partial result: 720
+Thread 3 calculated partial result: 210
+10! mod 17 = 14
+
+./factorial -k 10 --pnum=2 --mod=17
+Thread 1 calculated partial result: 120
+Thread 2 calculated partial result: 30240
+10! mod 17 = 14
+
+P.S. 
+Ещё можно добавить вывод итогового значения факториала, но я об этом подумал уже когда все сделал :D
+В целом, правильность вычислений легко проверить, если просто перемножить результаты, которые получили потоки
+
+3.
+gcc -o deadlock deadlock.c -lpthread
+./deadlock
+
+Thread 1: Holding mutex1
+Thread 2: Holding mutex2
+
+...
+...
+...
+
+Все зависает. Нужно самому завершать процесс
---
+++ lab5/src/factorial.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <getopt.h>
+
+// Мьютекс для обеспечения безопасности доступа к глобальным данным
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+long long result = 1; // Результат факториала
+
+// Структура для передачи параметров в поток
+struct ThreadData {
+    int start;         // Число для вычисления факториала
+    int end;
+    int thread_id;     // Идентификатор потока
+};
+
+void partial_factorial(void *args) {
+  struct ThreadData *data = (struct ThreadData *)args;
+  int start = data->start;
+  int end = data->end;
+  int thread_id = data->thread_id;
+
+  long long partial_result = 1;
+
+  // Вычисление части факториала для данного потока
+  for (int i = start; i <= end; i++) {
+    partial_result = (partial_result * i);
+  }
+
+  // Захватываем мьютекс для обновления глобального результата
+  pthread_mutex_lock(&mutex);
+  result = (result * partial_result);
+  pthread_mutex_unlock(&mutex);
+
+  printf("Thread %d calculated partial result: %lld\n", thread_id, partial_result);
+
+  pthread_exit(NULL);
+}
+
+int main(int argc, char* argv[]) {
+  int k = -1; // число, факториал которого необходимо вычислить
+  int pnum = -1; // количество потоков
+  int mod = -1; // модуль факториала
+
+  while (1) {
+    int option_index = 0;
+    static struct option long_options[] = {
+      {"k", required_argument, 0, 'k'},
+      {"pnum", required_argument, 0, 'p'},
+      {"mod", required_argument, 0, 'm'},
+      {0, 0, 0, 0}};
+
+    int c = getopt_long(argc, argv, "k:p:m:", long_options, &option_index);
+
+    if (c == -1) break;
+
+    switch (c) {
+      case 'k':
+        sscanf(optarg, "%d", &k);
+        break;
+      case 'p':
+        sscanf(optarg, "%d", &pnum);
+        break;
+      case 'm':
+        sscanf(optarg, "%d", &mod);
+        break;
+      default:
+        fprintf(stderr, "Usage: %s -k <number> --pnum=<threads> --mod=<modulus>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+  }
+
+  // Проверка наличия всех необходимых параметров
+  if (k == -1 || pnum == -1 || mod == -1) {
+    fprintf(stderr, "Usage: %s -k <number> --pnum=<threads> --mod=<modulus>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  // Массив идентификаторов потоков
+  pthread_t threads[pnum];
+  // Массив структур для передачи параметров в потоки
+  struct ThreadData thread_data[pnum];
+
+  // Создание потоков
+
+  int start_counter = 1;
+  int end_counter = 1;
+
+  int pnum_counter = pnum; // 4
+  int k_counter = k; // 10
+
+  for (int i = 0; i < pnum; i++) {
+    end_counter = start_counter + k_counter / pnum_counter - 1;
+
+    pnum_counter -= 1;
+    k_counter -= (end_counter - start_counter + 1);
+
+    thread_data[i].start = start_counter;
+    thread_data[i].end = end_counter;
+    thread_data[i].thread_id = i + 1;
+
+    if (pthread_create(&threads[i], NULL, (void *(*)(void *))partial_factorial, (void *)&thread_data[i]) != 0) {
+        perror("pthread_create");
+        exit(EXIT_FAILURE);
+    }
+
+    start_counter = end_counter + 1;
+  }
+
+  // Ожидание завершения всех потоков
+  for (int i = 0; i < pnum; i++) {
+    if (pthread_join(threads[i], NULL) != 0) {
+      perror("pthread_join");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  // Захватываем мьютекс для безопасного доступа к результату
+  pthread_mutex_lock(&mutex);
+  long long final_result = result % mod;
+  pthread_mutex_unlock(&mutex);
+
+  // Вывод результата
+  printf("%d! mod %d = %lld\n", k, mod, final_result);
+
+  // Уничтожение мьютекса
+  pthread_mutex_destroy(&mutex);
+
+  return 0;
+}
